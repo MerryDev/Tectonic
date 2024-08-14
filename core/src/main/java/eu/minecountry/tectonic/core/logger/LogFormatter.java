eaa526069b16f7cccaa402228cbeff70838b3cd1
Merry <svgr1808@outlook.de>
2024-08-14 16:38:57 +0200

    Fix spelling

--- core/src/main/java/eu/minecountry/tectonic/core/logger/LogFormatter.java
+++ core/src/main/java/eu/minecountry/tectonic/core/logger/LogFormatter.java
@@ -7,7 +7,7 @@ import java.util.TimeZone;
 import java.util.logging.Formatter;
 import java.util.logging.LogRecord;
 
-public class LofFormatter extends Formatter {
+public class LogFormatter extends Formatter {
 
     @Override
     public String format(LogRecord record) {
